435d1aaf46b9cb2b658d318cbbc0bc178b049c2b
Fishixz <158246871+fishixz@users.noreply.github.com>
2026-09-17 12:06:34 -0300

    Reload all RochaSystem slash commands

--- rocha-ticket-bot/src/system/commands/owner/recarregarComandos.js
+++ rocha-ticket-bot/src/system/commands/owner/recarregarComandos.js
@@ -1,14 +1,16 @@
 const { registerGuildCommands } = require('../../../commands/registerCommands');
+const { registerExtraGuildCommands } = require('../../../commands/extraCommands');
 const { sendSystemAudit } = require('../../../services/systemAuditService');
 
 async function execute(interaction) {
   await registerGuildCommands(interaction.guild);
+  await registerExtraGuildCommands(interaction.guild);
   await sendSystemAudit(interaction.guild, {
     action: 'Comandos recarregados',
     actor: interaction.user,
-    details: 'Os slash commands do RochaSystem foram registrados novamente neste servidor.'
+    details: 'Todos os slash commands do RochaSystem foram registrados novamente neste servidor.'
   });
-  return interaction.editReply('✅ Os comandos do RochaSystem foram registrados novamente neste servidor.');
+  return interaction.editReply('✅ Todos os comandos do RochaSystem foram registrados novamente neste servidor.');
 }
 
 module.exports = { id: 'dono.recarregar-comandos', execute };
